2874a5bd51393824ac50b208e5fff8f2aa4fa629
Pelane Sanon <p_sanon@aol.com>
2018-07-31 11:32:29 -0500

    Check in

---
+++ jscriptEx13.js
@@ -0,0 +1,18 @@
+
+function planet()
+{
+    alert("World");
+}
+
+function greeting()
+{
+    alert("Hello");
+}
+
+function main()
+{
+    greeting();
+    planet();
+}
+
+main();
---
+++ jscriptEx14.js
@@ -0,0 +1,13 @@
+function greeting(teacher)
+{
+    alert("My favorite teacher is "+teacher);
+}
+
+
+function main()
+{
+    var favoriteTeacher = "Kevin"
+    greeting(favoriteTeacher)
+}
+
+main();
